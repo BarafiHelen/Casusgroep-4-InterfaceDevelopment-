51f03de66aca2ca157d711c7bfaae8d104234bf0
redroze <helen.baravi94@gmail.com>
2025-06-05 02:42:11 +0200

    Orders page aanpassen met bootstrap

--- KE03_INTDEV_SE_2_Base/Controllers/OrdersController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/OrdersController.cs
@@ -16,12 +16,14 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
         private readonly MatrixIncDbContext _context;
         private readonly IProductRepository _productRepo;
         private readonly IOrderItemRepository _orderItemRepo;
+        private readonly ICustomerRepository _customerRepo;
 
-        public OrdersController(MatrixIncDbContext context, IProductRepository productRepo, IOrderItemRepository orderItemRepo)
+        public OrdersController(MatrixIncDbContext context, IProductRepository productRepo, IOrderItemRepository orderItemRepo, ICustomerRepository customerRepo)
         {
             _context = context;
             _productRepo = productRepo;
             _orderItemRepo = orderItemRepo;
+            _customerRepo = customerRepo;
         }
 
         // GET: Orders
@@ -87,6 +89,42 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
             }
 
             return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null) return NotFound();
+
+            ViewBag.CustomerList = new SelectList(_customerRepo.GetAllCustomers(), "Id", "Name", order.CustomerId);
+            return View(order);
+        }
+        // POST: Orders/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Order order)
+        {
+            if (id != order.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id)) return NotFound();
+                    else throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.CustomerList = new SelectList(_customerRepo.GetAllCustomers(), "Id", "Name", order.CustomerId);
+            return View(order);
         }
 
         // GET: Orders/Delete/5
